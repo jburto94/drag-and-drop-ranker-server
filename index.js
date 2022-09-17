71b4d06d8be926d1689fa1258417daf24bcab68b
unknown <jburto94@yahoo.com>
2022-09-17 15:10:45 -0400

    Connected to database

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 
 const app = express();
+
+const db = require('./src/db/db');
+db();
 
 app.get('/', (req, res) => {
   res.json('this is the root route')
---
+++ src/db/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const connect = async () => {
+  try {
+    const connection = await mongoose.connect(process.env.MONGODB_URI);
+    console.log('Connected to DB');
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+}
+
+module.exports = connect;
