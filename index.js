bae58e377a6823bdb5a918b592b0b86c6dcfc18e
unknown <jburto94@yahoo.com>
2022-09-23 02:44:22 -0400

    adjusted app listen

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const express = require('express');
-const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const usersRouter = require('./src/controllers/users');
@@ -8,13 +7,14 @@ const loginRouter = require('./src/controllers/login');
 const verifyTokenRouter = require('./src/controllers/verifyToken');
 const verifyEmailRouter = require('./src/controllers/verifyEmail');
 const listsRouter = require('./src/controllers/lists');
-
-const db = require('./src/db/db');
-db();
+const app = express();
 
 app.use(cors());
 app.use(bodyParser.json({}));
 app.use(bodyParser.urlencoded({ extended: true }));
+
+const db = require('./src/db/db');
+db();
 
 app.use('/api/users', usersRouter);
 app.use('/api/login', loginRouter);
