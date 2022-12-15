383d508bc7f9d3f52e6527a0b2acb5b1cb51fbbd
unknown <jburto94@yahoo.com>
2022-12-14 22:12:19 -0500

    test

--- index.js
+++ index.js
@@ -2,21 +2,25 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+
+const usersRouter = require('./src/controllers/users');
+const loginRouter = require('./src/controllers/login');
+const verifyTokenRouter = require('./src/controllers/verifyToken');
 const listsRouter = require('./src/controllers/lists');
+const verifyEmailRouter = require('./src/controllers/verifyEmail');
+
 const app = express();
 
+app.use(cors());
 app.options('*', cors());
+
+console.log('cors working?');
 
 app.use(bodyParser.json({}));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const db = require('./src/db/db');
 db();
-
-const usersRouter = require('./src/controllers/users');
-const loginRouter = require('./src/controllers/login');
-const verifyTokenRouter = require('./src/controllers/verifyToken');
-const verifyEmailRouter = require('./src/controllers/verifyEmail');
 
 app.use('/api/users', usersRouter);
 app.use('/api/login', loginRouter);
